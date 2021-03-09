204f1913ef076c63cb05177edb5e511bef8df2cd
eya-98 <eya.nani@sesame.com.tn>
2021-03-09 16:48:16 +0000

    binary trees

--- binary_trees.h
+++ binary_trees.h
@@ -1,3 +1,5 @@
+#ifndef BINARYTREE
+#define BINARYTREE
 /**
  * struct binary_tree_s - Binary tree node
  *
@@ -13,5 +15,10 @@ struct binary_tree_s
     struct binary_tree_s *left;
     struct binary_tree_s *right;
 };
-
 typedef struct binary_tree_s binary_tree_t;
+typedef struct binary_tree_s bst_t;
+typedef struct binary_tree_s avl_t;
+typedef struct binary_tree_s heap_t;
+void binary_tree_print(const binary_tree_t *);
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
+#endif
