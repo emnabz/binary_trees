3bff2db789b23fbb1c6a9adffa3781d09f11e428
emna <1947@holbertonschool.com>
2021-03-09 19:41:58 +0100

    left-chlid node

---
+++ 1-binary_tree_insert_left.c
@@ -0,0 +1,26 @@
+#include "binary_trees.h"
+/**
+* binary_tree_node - creates a binary tree node
+* @parent: parent of the node to create
+* @value: value to store in new node
+*
+* return: a pointer the new created node
+*/
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
+{
+binary_tree_t *node;
+if (!parent)
+return (NULL);
+node = malloc(sizeof(binary_tree_t));
+if (!node)
+return (NULL);
+node->left = parent->left;
+if (parent->left)
+parent->left->parent = node;
+node->parent = parent;
+node->left = node;
+node->right = NULL;
+node->n = value;
+return (node);
+
+}
