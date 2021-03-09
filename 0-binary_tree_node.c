b8b1cddc42d52f9ad1d9d7fc7f2e0d33b2a2e67d
eya-98 <eya.nani@sesame.com.tn>
2021-03-09 17:18:00 +0000

    binary tree

---
+++ 0-binary_tree_node.c
@@ -0,0 +1,16 @@
+#include "binary_trees.h"
+/**
+* 
+*
+*/
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+    binary_tree_t *node;
+    node = malloc(sizeof(binary_tree_t));
+    if (node == NULL)
+    return (NULL);
+    node->parent = parent;
+    node->n = value;
+    return (node);
+}
+
