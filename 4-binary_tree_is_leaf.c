1dfc00d8fbead533b0cb4ce8f2308b976516c26d
emna <1947@holbertonschool.com>
2021-03-09 21:46:29 +0100

    task4

---
+++ 4-binary_tree_is_leaf.c
@@ -0,0 +1,17 @@
+#include "binary_trees.h"
+/**
+* binary_tree_is_leaf - checks if a node is a leaf
+* @node: a pointer to the root of binary tree
+*
+* return: node
+*/
+int binary_tree_is_leaf(const binary_tree_t *node)
+{
+if (!node)
+return (0);
+if (node->right || node->left)
+{
+return (0);
+}
+return (1);
+}
