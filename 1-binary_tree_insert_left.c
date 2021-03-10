1fcd591a410ab63cd460273fe39b767c46567a5b
emna <1947@holbertonschool.com>
2021-03-10 17:55:37 +0100

    task1

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -9,18 +9,19 @@
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
 binary_tree_t *node;
-if (!parent)
+if (parent == NULL)
 return (NULL);
 node = malloc(sizeof(binary_tree_t));
-if (!node)
+if (node == NULL)
 return (NULL);
+if (parent->left != NULL)
+{
+parent->left = parent->left;
 node->left = parent->left;
-if (parent->left)
 parent->left->parent = node;
+}
+parent->left = node;
 node->parent = parent;
-node->left = node;
-node->right = NULL;
 node->n = value;
-return (node);donh
-
+return (node);
 }
