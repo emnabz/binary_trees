40c286cf53fb8cadc40ec767b55fafde99c51b33
emna <1947@holbertonschool.com>
2021-03-10 11:42:54 +0100

    task8

--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -3,7 +3,7 @@
 * binary_tree_postorder - a binary tree using post-order traversal
 * @tree: a pointer to the root node of the tree to traverse
 * @func: a pointer to a function to call for each node
-* return: void
+* Return: void
 */
 void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
 {
